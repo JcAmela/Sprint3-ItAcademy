65b7f2e63566a51195685aa714ceb4d0622eaff3
JcAmela <juancarlosamela@gmail.com>
2023-05-30 19:19:50 +0200

    validacion a medias

--- js/checkout.js
+++ js/checkout.js
@@ -1,31 +1,33 @@
 
 // Exercise 6
-function validate() {
+function validate(event) {
 	var error = 0;
-
+	event.preventDefault();
 	// Get the error elements
 	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
+	var errorEmail = document.getElementById("errorEmail");
+
 	// Comprobamos si todos los campos estan rellenados ademas de si tienen al menos 3 caracteres.
-	var fieldForm = ["fName", "fEmail", "fLastN", "fPaswword", "fAdress", "fPhone"];
+	var fieldForm = ["fName", "fEmail", "fLastN", "fPassword", "fAddress", "fPhone"];
 	var lettersOnly = /^[A-Za-z]+$/;
 	fieldForm.forEach(element => {
 		let field = document.getElementById(element)
 		if (field.value.length <= 3) {
 			error++
 		}
-		// esto hay que cambiar los fname ponerlos en una variable
-		if (!lettersOnly.test(fieldForm[0].value) || !lettersOnly.test(fieldForm[1].value)) {
-            error++;	
+		// Comprobamos si los campos se han escrito con los caracteres adecuados
+		if (lettersOnly.test(field.value)) {
+			error++
 		}
 	});
 
-	
-	 
-	if(error>=1){
+
+
+
+	//  Comprobamos si hay errores y mostramos el mensaje.
+	if (error >= 1) {
 		alert("Error");
-	}else{
+	} else {
 		alert("OK");
 	}
 
